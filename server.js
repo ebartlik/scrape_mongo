bec3736f3375a5d4b22ed92ef9dc2845e7607ded
ebartlik <ebartlik@gmail.com>
2018-10-02 20:17:21 -0400

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var MONGODB_URI = process.env.MONGODB_URI 
 
 
 
